4ea3a89a95e79fc9cdd34e06449c3ea7e6af2e84
OTCelbakai <selbakai@otcengineering.com>
2024-04-11 17:31:12 +0200

    My first commit

---
+++ L_1/main.c
@@ -0,0 +1,28 @@
+
+int main()
+{
+    unsigned int counter = 0;
+    unsigned int a = 0;
+  while(1){
+
+       counter = 1U;
+       a = counter;
+       counter = 10U;
+       a = counter;
+       counter = 2000000000U;
+       a = counter;
+       counter = 3000000000U;
+       a = counter;
+       counter = 2000000000;
+       a = counter;
+       counter = 3000000000;
+       a = counter;
+       counter = -1U;
+       a = counter;
+       a++;
+       
+
+      return 0;
+  }
+
+}
